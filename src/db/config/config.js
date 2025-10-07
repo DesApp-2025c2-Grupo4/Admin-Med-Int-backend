b78e8530fc2b475b78fd969e264a29c87380859a
Alvaro Bravo <bravoalva19@gmail.com>
2025-10-07 14:30:32 -0300

    Se agregó correctamente conexion de la base de datos

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -6,7 +6,7 @@ dotenv.config()
 const DB_USER = process.env.DB_USER || 'admin'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'admin123'
 const DB_NAME = process.env.DB_NAME || 'db_admin_med_integral'
-const DB_HOST = process.env.DB_HOST || 'localhost' // o el nombre del servicio en docker-compose
+const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_PORT = process.env.DB_PORT || 5431
 const DB_DIALECT = process.env.DB_DIALECT || 'postgres'
 
@@ -17,7 +17,7 @@ module.exports = {
     password: DB_PASSWORD,
     database: DB_NAME,
     host: DB_HOST,
-    port: DB_PORT,
+    port: Number(DB_PORT),
     dialect: DB_DIALECT
   },
   test: {
