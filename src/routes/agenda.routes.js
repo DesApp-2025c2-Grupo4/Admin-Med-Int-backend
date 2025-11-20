488add6aeb4d100c23df4c7684234992e7ee9281
rpagliarulo <rocioyasmin.pagliarulo@estudiantes.unahur.edu.ar>
2025-11-20 18:03:24 -0300

    middleware validarAgenda

--- src/routes/agenda.routes.js
+++ src/routes/agenda.routes.js
@@ -3,15 +3,16 @@ const { agendaControllers } = require('../controllers')
 const agendaRoutes = Router()
 const cacheMiddleware  = require('../middleware/redisMiddleware.js')
 const validarAgenda = require('../middleware/validarAgenda.js')
+const validarHorario = require('../middleware/validarHorario.js')
 
 agendaRoutes.get('/', cacheMiddleware.checkCache('agenda:list'), agendaControllers.getAgendas)
 
-agendaRoutes.post('/', cacheMiddleware.deleteCache('agenda:list'), validarAgenda ,agendaControllers.createAgenda )
+agendaRoutes.post('/', cacheMiddleware.deleteCache('agenda:list'), validarAgenda, validarHorario  ,agendaControllers.createAgenda )
 
 agendaRoutes.delete('/:id', agendaControllers.eliminarUnaAgenda)
 
 agendaRoutes.get('/:id', agendaControllers.getAgendaById)
 
-agendaRoutes.put('/:id', validarAgenda ,agendaControllers.updateAgenda)
+agendaRoutes.put('/:id', validarAgenda, validarHorario ,agendaControllers.updateAgenda)
 
 module.exports = agendaRoutes
